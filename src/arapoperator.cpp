4808dca155e2b9c21a47c4a3b858bd20e5d97179
dongmo <dong_mo@163.com>
2021-05-08 22:31:40 +0800

    20210508 bakcup

--- src/arapoperator.cpp
+++ src/arapoperator.cpp
@@ -13,15 +13,21 @@ void ArapOperator::Deformation(
   MatrixXi T, surface;
   VectorXi b;
   transform_hex_to_matrix(V, T, b, bc, surface, _ovm, fixed);
+  // printEigenMatrix(bc, std::cout);
   MatrixXd V0 = V;
+  igl::SLIMData sData;
+  igl::slim_precompute(V, T, V0, sData, igl::SYMMETRIC_DIRICHLET, b, bc, 1e6);
+  igl::slim_solve(sData, 5);
+  transform_matrix_to_hex(sData.V_o, _ovm);
   // slim
+  /*
   igl::my_scaf::SCAFData sData;
   igl::my_scaf::scaf_precompute(V, T, V0, surface, sData,
                                 igl::MappingEnergyType::SYMMETRIC_DIRICHLET, b,
-                                bc, 1e6);
-  V0 = igl::my_scaf::scaf_solve(sData, 3);
+                                bc, 1e9);
+  V0 = igl::my_scaf::scaf_solve(sData, 5);
+  */
   // slim完了
-  transform_matrix_to_hex(V0, _ovm);
 }
 
 void ArapOperator::Optimize(OpenVolumeMesh::GeometricHexahedralMeshV3d &_ovm,
@@ -39,6 +45,6 @@ void ArapOperator::Optimize(OpenVolumeMesh::GeometricHexahedralMeshV3d &_ovm,
   igl::my_scaf::scaf_precompute(V, T, V_o, surface, data,
                                 igl::MappingEnergyType::SYMMETRIC_DIRICHLET, b,
                                 bc, 1e6);
-  V_o = igl::my_scaf::scaf_solve(data, 3);
+  V_o = igl::my_scaf::scaf_solve(data, 5);
   transform_matrix_to_hex(V_o, _ovm);
 }
